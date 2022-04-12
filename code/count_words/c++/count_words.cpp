1503b356109de7275e87666db3d1f016690234dd
Lukas Wagner <lwagner94@posteo.at>
2022-04-12 14:27:21 +0200

    Count words in C and C++

---
+++ code/count_words/c++/count_words.cpp
@@ -0,0 +1,44 @@
+
+#include <string>
+#include <iostream>
+#include <fstream>
+
+// Count the words in the file "path" and return the number of words.
+int count_words(std::string path) {
+    std::ifstream file(path);
+
+    if (!file.good()) {
+        return -1;
+    }
+
+    std::string content( (std::istreambuf_iterator<char>(file) ),
+                       (std::istreambuf_iterator<char>()) );
+
+    bool inword = false;
+    int counted = 0;
+
+    for (auto &ch : content) {
+        if (isalnum(ch)) {
+            if (!inword) {
+                inword = true;
+                counted++;
+            }
+        } else {
+            inword = false;
+        }
+    }
+
+
+    return counted;
+
+}
+
+
+
+int main(int argc, char *argv[]) {
+    int empty_words1 = count_words("files/empty.txt");
+    int empty_words2 = count_words("files/empty_with_space.txt");
+    int ten_words = count_words("files/10_words.txt");  
+    int single_word = count_words("files/single_word_end.txt");      
+    return empty_words1 != 0 || empty_words2 != 0 || ten_words != 10 || single_word != 1;
+}
---
+++ code/count_words/c/count_words.c
@@ -0,0 +1,64 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+int count_words(const char *path) {
+    FILE* f = fopen(path, "r");
+    if (f == NULL) {
+        goto out;
+    }
+
+    if (fseek (f, 0, SEEK_END) != 0) {
+        goto out;
+    }
+    long length = ftell (f);
+
+    if (length == -1) {
+        goto out;
+    }
+
+    if (fseek (f, 0, SEEK_SET) != 0) {
+        goto out;
+    }
+
+    char* buffer = malloc (length);
+    if (!buffer) {
+        goto out;
+    }
+    if ( fread (buffer, 1, length, f) != length) {
+        goto out;
+    }
+
+    int counted = 0;
+    bool inword = false;
+
+    for (int i = 0; i < length; i++) {
+        char c = buffer[i];
+
+        if (isalnum(c)) {
+            if (!inword) {
+                inword = true;
+                counted++;
+            }
+        } else {
+            inword = false;
+        }
+    }
+
+    return counted;
+
+out:
+    fclose(f);
+    free(buffer);
+    return -1;
+}
+
+int main(int argc, char *argv[]) {
+    int empty_words1 = count_words("files/empty.txt");
+    int empty_words2 = count_words("files/empty_with_space.txt");
+    int ten_words = count_words("files/10_words.txt");  
+    int single_word = count_words("files/single_word_end.txt");      
+    return empty_words1 != 0 || empty_words2 != 0 || ten_words != 10 || single_word != 1;
+}
